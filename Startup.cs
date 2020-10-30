578851a73d66df26754fca81cdb71fff06bd6fc1
RamonaEJSpence <ramonaejspence@gmail.com>
2020-10-30 11:58:25 -0400

    generate Movie Controller | edit startup to use sqlite in dev and sqlserver in prod

--- Startup.cs
+++ Startup.cs
@@ -8,22 +8,40 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MovieMVC
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MovieMvcContext>(options => 
+            {
+                var connectionString = Configuration.GetConnectionString("MovieMvcContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
